604dd2e059f9a4cd70f6dcf05138d3bec4da4c23
Daynamarie <Dayna.christison@gmail.com>
2020-09-19 12:34:00 -0400

    rendering movie id title

--- client/src/screens/MovieDetail.jsx
+++ client/src/screens/MovieDetail.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { getOneMovie } from '../services/movies'
 
-const MovieDetail = () => {
+const MovieDetail = (props) => {
+const {reviews, handleDelete, updateSubmit} = props
+
+const [movie, setMovie] = useState(null)
+
+const {id} = useParams()
+
+useEffect (() => {
+    const fetchMovie = async () => {
+        const singleMovie = await getOneMovie(id)
+        setMovie(singleMovie)
+    }
+    fetchMovie()
+}, [])
 
     return(
         <div>
-            <h2>SPECIFIC MOVIE HERE PEOPLE!</h2>
+            {
+                movie &&
+                <>
+                    <h2>{movie.title}</h2>
+                </>
+            }
         </div>
     )
 }
