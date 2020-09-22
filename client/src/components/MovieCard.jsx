c1a1fb69df955740a8c84ac01713da7e01f9da3d
Daynamarie <Dayna.christison@gmail.com>
2020-09-22 03:54:09 -0400

    boxes done

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Header from "./Header";
 
 import './MovieCard.css'
 
@@ -11,25 +12,35 @@ const MovieCard = (props) => {
     return(
     <div className='movie-card'key={movie.id}>
         <Link className='card' to={`/movies/${movie.id}`}>
-            <div className='detail-section'>
-              <h2 className='break'>{movie.title}</h2>
-            </div>
+            
+              <h3 className='break'>{movie.title}</h3>
+         
             <div className='detail-section'>
               <h4>Directed By:</h4>
-              <h3>{movie.director}</h3>
+              <h3 className='movie-value'>{movie.director}</h3>
             </div>
             
             <img className='movie-card-image' src={movie.img_url} alt={movie.title} />
-            <h4>Produced By:</h4>
-            <h3>{movie.producer}</h3>
-            <h4>Written By:</h4>
-            <h3>{movie.writer}</h3>
-            <h4>Screenplay By:</h4>
-            <h3>{movie.screenplay_by}</h3>
-            <h4>Year Released</h4>
-            <h3>{movie.year_released}</h3>
-            <h4>Genre</h4>
-            <h3>{movie.genre}</h3>
+            <div className='detail-section'>
+              <h4>Produced By:</h4>
+              <h3 className='movie-value'>{movie.producer}</h3>
+            </div>
+            <div className='detail-section'>
+              <h4>Written By:</h4>
+              <h3 className='movie-value'>{movie.writer}</h3>
+            </div>
+            <div className='detail-section'>
+              <h4>Screenplay By:</h4>
+              <h3 className='movie-value'>{movie.screenplay_by}</h3>
+            </div>
+            <div className='detail-section'>
+              <h4>Year Released:</h4>
+              <h3 className='movie-value'>{movie.year_released}</h3>
+            </div>
+            <div className='genre-section'>
+              <h4>Genre:</h4>
+              <h3 className='movie-value-genre'>{movie.genre}</h3>
+            </div>
         </Link>
     </div>
     )}
