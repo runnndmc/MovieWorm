8aea1492a9c1792702f6fe948f6d9c5e42b3d791
Daynamarie <Dayna.christison@gmail.com>
2020-11-24 14:01:32 -0500

    favicon change and footer change

--- client/src/screens/MovieEdit.jsx
+++ client/src/screens/MovieEdit.jsx
@@ -1,25 +1,45 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-import { getOneMovie } from '../services/movies';
-import './MovieEdit.css'
+import { getOneMovie } from "../services/movies";
+import "./MovieEdit.css";
 
 const MovieEdit = (props) => {
-    useEffect(() => {
-        window.scrollTo(0, 0);
-      }, []);
-    const [formData, setFormData] = useState({
-        title: '',
-        director: "",
-        producer: "",
-        writer: "",
-        screenplay_by: "",
-        year_released: "",
-        genre: "",
-        img_url: "",
-    })
+    
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  const [formData, setFormData] = useState({
+    title: "",
+    director: "",
+    producer: "",
+    writer: "",
+    screenplay_by: "",
+    year_released: "",
+    genre: "",
+    img_url: "",
+  });
 
-    const {
+  const {
+    title,
+    director,
+    producer,
+    writer,
+    screenplay_by,
+    year_released,
+    genre,
+    img_url,
+  } = formData;
+
+  console.log();
+
+  let { id } = useParams();
+  const { updateSubmit } = props;
+
+  useEffect(() => {
+    const prefillForm = async () => {
+      const findOne = await getOneMovie(id);
+      const {
         title,
         director,
         producer,
@@ -28,121 +48,84 @@ const MovieEdit = (props) => {
         year_released,
         genre,
         img_url,
-      } = formData;
+      } = findOne;
+      setFormData({
+        title,
+        director,
+        producer,
+        writer,
+        screenplay_by,
+        year_released,
+        genre,
+        img_url,
+      });
+    };
+    prefillForm();
+  }, []);
 
-      console.log()
+  const handleChange = (e) => {
+    const { value, name } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
+  return (
+    <form
+      className="edit-form"
+      onSubmit={(e) => {
+        e.preventDefault();
+        updateSubmit(id, formData);
+      }}
+    >
+      <h2 className="edit-title">Edit Your Mistakes</h2>
+      <label>Title:</label>
+      <input name="title" type="text" value={title} onChange={handleChange} />
+      <label>Director:</label>
+      <input
+        name="director"
+        type="text"
+        value={director}
+        onChange={handleChange}
+      />
+      <label>Producer:</label>
+      <input
+        name="producer"
+        type="text"
+        value={producer}
+        onChange={handleChange}
+      />
+      <label>Writer:</label>
+      <input name="writer" type="text" value={writer} onChange={handleChange} />
+      <label>Screenplay By:</label>
+      <input
+        name="screenplay_by"
+        type="text"
+        value={screenplay_by}
+        onChange={handleChange}
+      />
+      <label>Year Released:</label>
+      <input
+        name="year_released"
+        type="number"
+        value={year_released}
+        onChange={handleChange}
+      />
+      <label>Genre:</label>
+      <input name="genre" type="text" value={genre} onChange={handleChange} />
+      <label>Movie Image URL</label>
+      <input
+        name="img_url"
+        type="text"
+        value={img_url}
+        onChange={handleChange}
+      />
+      <div className="save-button-container">
+        <button className="hit-save">Save</button>
+      </div>
+    </form>
+  );
+};
 
-      let {id} = useParams()
-      const { updateSubmit } = props
-
-      useEffect(()=> {
-        const prefillForm = async () => {
-            const findOne = await getOneMovie(id)
-            const {
-                title,
-                director,
-                producer,
-                writer,
-                screenplay_by,
-                year_released,
-                genre,
-                img_url,
-              } = findOne;
-            setFormData({
-                title,
-                director,
-                producer,
-                writer,
-                screenplay_by,
-                year_released,
-                genre,
-                img_url,
-              })
-            } 
-            prefillForm()
-        }, [])
-
-
-        const handleChange = (e) => {
-            const {value, name} = e.target
-            setFormData(prevState => ({
-                ...prevState,
-                [name]:value
-            }))
-        }
-      
-
-    return (
-        <form 
-            className='edit-form'
-            onSubmit={(e) => {
-            e.preventDefault() 
-            updateSubmit(id, formData)
-        }}>
-            <h2 className='edit-title'>Edit Your Mistakes</h2>
-            <label>Title:</label>
-        <input
-            name='title'
-            type='text'
-            value={title}
-            onChange={handleChange}
-        />
-        <label>Director:</label>
-        <input
-            name='director'
-            type='text'
-            value={director}
-            onChange={handleChange}
-        />
-        <label>Producer:</label>
-        <input
-            name='producer'
-            type='text'
-            value={producer}
-            onChange={handleChange}
-        />
-        <label>Writer:</label>
-        <input 
-            name='writer'
-            type='text'
-            value={writer}
-            onChange={handleChange}
-        />
-        <label>Screenplay By:</label>
-        <input 
-            name='screenplay_by'
-            type='text'
-            value={screenplay_by}
-            onChange={handleChange}
-        />
-        <label>Year Released:</label>
-        <input
-            name='year_released'
-            type='number'
-            value={year_released}
-            onChange={handleChange}
-        />
-        <label>Genre:</label>
-        <input
-            name='genre'
-            type='text'
-            value={genre}
-            onChange={handleChange}
-        />
-        <label>Movie Image URL</label>
-        <input
-            name='img_url'
-            type='text'
-            value={img_url}
-            onChange={handleChange}
-        />
-        <div className='save-button-container'>
-            <button className='hit-save'>Save</button>
-        </div>
-       
-        </form>
-    )
-}
-
-export default MovieEdit
+export default MovieEdit;
